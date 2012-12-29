c602ce941d268e3a8497f5f58f5800a58945d9c0
Jijo <jijo.thomas@gmail.com>
2012-12-29 09:34:42 -0500

    angularised sudoku

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
-    Redirect(routes.SudokuController.index(0))
+    Ok(views.html.index("Hi"))
   }
   
 }
--- app/controllers/SudokuController.scala
+++ app/controllers/SudokuController.scala
@@ -4,13 +4,28 @@ import play.api._
 import play.api.mvc._
 import play.api.mvc.Results.Ok
 import generator.SudokuGenerator
+import play.api.libs.json.Json
+import play.api.libs.json.Writes
+import play.api.libs.json.JsArray
+import play.api.libs.json.JsArray
+import play.api.libs.json.JsString
 
 
 object SudokuController {
 
 
+  implicit object RowWrites extends Writes[List[Char]] {
+    def writes(row:List[Char]) = JsArray(row.map(c => JsString(c.toString)))
+  }
+  
+  implicit object GridWrites extends Writes[List[List[Char]]] {
+    def writes(grid:List[List[Char]]) = JsArray(grid.map(row => Json.toJson(row)(RowWrites)))
+  }
+  
   def index(level:Int=1) = Action {
     val (solution,puzzle) = SudokuGenerator.createPuzzle(level%3)
-    Ok(views.html.sudoku.index(puzzle))
+    
+   val jsonSudoku = Json.toJson(puzzle)(GridWrites)
+    Ok(jsonSudoku)
   }
 }
